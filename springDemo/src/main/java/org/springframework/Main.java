f5074951f6aa92174fd997c76bd9431227061c58
DJlv <2743535685@qq.com>
2024-07-14 14:46:33 +0800

    init

--- springDemo/src/main/java/org/springframework/Main.java
+++ springDemo/src/main/java/org/springframework/Main.java
@@ -8,43 +8,5 @@ public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context =
 				new AnnotationConfigApplicationContext(BeanConfigAll.class);
-
-//		for (String name : context.getBeanDefinitionNames()) {
-//			System.out.println(name);
-//		}
-//
-//		System.out.println(context.getBean(Bean2.class).getBean1());
-//		System.out.println("skdjfksd");
-	}
-
-	@Configuration
-	static class Config {
-		@Bean
-		public Bean1 bean1() {
-			return new Bean1();
-		}
-
-		@Bean
-		public Bean2 bean2(Bean1 bean1) {
-			Bean2 bean2 = new Bean2();
-			bean2.setBean1(bean1);
-			return bean2;
-		}
-	}
-
-	static class Bean1 {
-	}
-
-	static class Bean2 {
-
-		private Bean1 bean1;
-
-		public void setBean1(Bean1 bean1) {
-			this.bean1 = bean1;
-		}
-
-		public Bean1 getBean1() {
-			return bean1;
-		}
 	}
 }
